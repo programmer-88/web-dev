2a9c64d120fa634956dbd08fefa19f30f34790da
Ian Mbai <ian@ians-probook-440-g5>
2023-03-06 20:52:14 +0300

    a python file

---
+++ subjects.py
@@ -0,0 +1,22 @@
+#read marks fot five subjects
+math = int(input("Enter marks for math:"))
+english = int(input("Enter marks for English:"))
+physics = int(input("Enter marks for Physics:"))
+chemistry = int(input("Enter marks for Chemistry:"))
+history = int(input("Enter marks for History:"))
+
+#sum of the subjects
+Sum = (math + english + physics + chemistry + history)
+
+#finding the average
+avg = Sum / 5
+
+#printing the sum and average 
+print("You have an sum of",Sum, "and an average of",avg,)
+
+#find if the user has passed or failed
+if avg >20 :
+    print("You have succesfully passed the test ;)")
+else :
+    print("You have not passed the test :()")
+
